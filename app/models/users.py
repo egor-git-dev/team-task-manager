af626ba92d1cef7e82bfcf1746b26f4c51b6134e
Egor Milehin <emilyohin@yandex.ru>
2026-06-21 13:20:37 +0300

    Add User model and UserRole test

---
+++ app/models/users.py
@@ -0,0 +1,36 @@
+from datetime import datetime
+
+from sqlalchemy.orm import Mapped, mapped_column
+from sqlalchemy import DateTime, Enum as SQLEnum, func
+from enum import Enum
+
+from app.db.base import Base
+
+
+class UserRole(str, Enum):
+    USER = "user"
+    MANAGER = "manager"
+    ADMIN = "admin"
+
+
+class User(Base):
+    __tablename__ = "users"
+
+    id: Mapped[int] = mapped_column(primary_key=True)
+    email: Mapped[str] = mapped_column(nullable=False, unique=True, index=True)
+    hashed_password: Mapped[str] = mapped_column(nullable=False)
+    is_active: Mapped[bool] = mapped_column(default=True, nullable=False)
+    role: Mapped[UserRole] = mapped_column(
+        SQLEnum(
+            UserRole,
+            name="user_role",
+            values_callable=lambda enum_cls: [item.value for item in enum_cls],
+        ),
+        default=UserRole.USER,
+        nullable=False,
+    )
+    created_at: Mapped[datetime] = mapped_column(
+        DateTime(timezone=True),
+        server_default=func.now(),
+        nullable=False,
+    )
